c53bfaf947a716af73e32e9c76589e7554fab3ed
wojlin <wojciech.linowski.2137@gmail.com>
2023-01-17 21:09:21 +0100

    generating cards deck

--- Cards.py
+++ Cards.py
@@ -1,13 +1,41 @@
 class Card:
-    def __init__(self, card_type: str, picture_url):
+    TUNNEL_TYPE = "Tunnel Card"
+    ACTION_TYPE = "Action Card"
+
+    def __init__(self, card_type: str, picture_url: str):
         self.card_type = card_type
         self.picture_url = picture_url
 
     def info(self):
-        return f"card type: {self.card_type}"
+        card_vis = ''
+        if self.card_type == self.TUNNEL_TYPE:
+            possible_cards = {"none_none_bottom_left": '╗',
+                              "top_none_none_left": '╝',
+                              "top_right_none_none": '╚',
+                              "none_right_bottom_none": '╔',
+                              "none_none_none_none": '╳',
+                              "top_right_bottom_left": '╬',
+                              "top_right_bottom_none": '╠',
+                              "top_none_bottom_left": '╣',
+                              "top_right_none_left": '╩',
+                              "none_right_bottom_left": '╦',
+                              "top_none_bottom_none": '║',
+                              "none_right_none_left": '═',
+                              "top_none_none_none": '╹',
+                              "none_right_none_none": '╺',
+                              "none_none_bottom_none": '╻',
+                              "none_none_none_left": '╸'}
+            card_vis = f"card visualization: [{possible_cards[self.picture_url[:-4]]}]"
+        elif self.card_type == self.ACTION_TYPE:
+            action_type = self.picture_url.split('_')[0]
+            points = self.picture_url.split('_')[1][:-4]
+            card_vis = f"action type: {action_type}  action effect: {points}"
+        else:
+            raise Exception("unknown card type")
+        return f"card type: {self.card_type}  {card_vis}"
 
 
-class DirectionCard(Card):
+class TunnelCard(Card):
     def __init__(self, way_top: bool,
                  way_right: bool,
                  way_bottom: bool,
@@ -24,7 +52,7 @@ class DirectionCard(Card):
         self.overwrite = overwrite
         self.__card_name = card_name
 
-        Card.__init__(self, "Tunnel Card", self.__create_filename())
+        Card.__init__(self, self.TUNNEL_TYPE, self.__create_filename())
 
     def __create_filename(self):
         if self.__card_name:
@@ -33,17 +61,17 @@ class DirectionCard(Card):
         filename += "top_" if self.way_top else "none_"
         filename += "right_" if self.way_right else "none_"
         filename += "bottom_" if self.way_bottom else "none_"
-        filename += "left_" if self.way_left else "none_"
+        filename += "left" if self.way_left else "none"
         filename += '.png'
         return filename
 
 
 class ActionCard(Card):
-    def __init__(self, action_type: str, action_points: int):
+    def __init__(self, action_type: str, is_positive_effect: bool):
         self.action_type = action_type
-        self.action_points = action_points
-        Card.__init__(self, "Action Card", self.__create_filename())
+        self.is_positive_effect = is_positive_effect
+        Card.__init__(self, self.ACTION_TYPE, self.__create_filename())
 
     def __create_filename(self):
-        return f"{self.action_type}_{'pos' if self.action_points > 0 else 'neg'}.png"
+        return f"{self.action_type}_{'positive' if self.is_positive_effect else 'negative'}.png"
 
--- GameEngine.py
+++ GameEngine.py
@@ -2,8 +2,9 @@ from typing import Dict
 import random
 import math
 
-from Cards import Card
+import Cards
 from Player import Player
+
 
 class GameEngine:
     def __init__(self, cells_x: int, cells_y: int, name: str, game_id: str, players: Dict[str, Player], config: dict):
@@ -13,6 +14,84 @@ class GameEngine:
         self.game_id = game_id
         self.players = players
         self.config = config
+
+        self.GOOD_PLAYER = "Solider"
+        self.BAD_PLAYER = "Saboteur"
+
+        self.ACTION_CARDS_TYPES_AMOUNT = 3
+
+        self.INITIAL_TUNNEL_CARDS_AMOUNT = 44
+        self.INITIAL_ACTION_CARDS_AMOUNT = 27
+        self.INITIAL_CARDS_AMOUNT = self.INITIAL_TUNNEL_CARDS_AMOUNT + self.INITIAL_ACTION_CARDS_AMOUNT
+
+        self.SABOTEUR_AMOUNT = self.__assign_saboteurs_amount()
+        self.CARDS_PER_PLAYER_AMOUNT = self.__assign_card_amount_per_player()
+
+        self.__assign_player_roles()
+        self.__assign_card_amount_per_player()
+
+        self.cards = self.__create_card_deck()
+
+        self.current_turn_num = 0
+        self.turn = list(self.players.values())[self.current_turn_num].player_id
+
+    def __assign_player_roles(self):
+        for player_id, player_obj in self.players.items():
+            player_obj.player_role = self.GOOD_PLAYER
+
+        saboteurs = []
+        while len(saboteurs) < self.SABOTEUR_AMOUNT:
+            player_id, player_obj = random.choice(list(self.players.items()))
+            if player_id not in saboteurs:
+                saboteurs.append(player_id)
+                player_obj.player_role = self.BAD_PLAYER
+
+    def __assign_saboteurs_amount(self):
+        return math.floor(len(self.players) / 2.1)
+
+    def __assign_card_amount_per_player(self):
+        x = len(self.players)
+        return 6 if 3 <= x <= 5 else 4 if 6 <= x <= 10 else 0
+
+    def __create_card_deck(self):
+        card_deck = []
+
+        for i in range(self.INITIAL_TUNNEL_CARDS_AMOUNT):
+            way_top = bool(random.getrandbits(1))
+            way_right = bool(random.getrandbits(1))
+            way_bottom = bool(random.getrandbits(1))
+            way_left = bool(random.getrandbits(1))
+            card_deck.append(Cards.TunnelCard(way_top=way_top,
+                                              way_right=way_right,
+                                              way_bottom=way_bottom,
+                                              way_left=way_left,
+                                              destructible=True,
+                                              overwrite=False))
+
+        for i in range(self.INITIAL_ACTION_CARDS_AMOUNT):
+            action_type = str(random.randint(1, self.ACTION_CARDS_TYPES_AMOUNT))
+            action_effect = bool(random.getrandbits(1))
+            card_deck.append(Cards.ActionCard(action_type=action_type, is_positive_effect=action_effect))
+
+        random.shuffle(card_deck)
+
+        return card_deck
+
+
+class Game(GameEngine):
+    def __init__(self, cells_x: int, cells_y: int, name: str, game_id: str, players: Dict[str, Player], config: dict):
+        GameEngine.__init__(self,
+                            cells_x=cells_x,
+                            cells_y=cells_y,
+                            name=name,
+                            game_id=game_id,
+                            players=players,
+                            config=config)
+
+    def end_turn(self):
+        self.current_turn_num = self.current_turn_num + 1 if self.current_turn_num < len(self.players) else 0
+        self.turn = list(self.players.values())[self.current_turn_num].player_id
+        return f"turn ended, now its '{self.players[self.turn].player_name}' turn"
 
     def check_game_creation_rules(self):
         if len(self.players) < 3:
@@ -37,45 +116,12 @@ class GameEngine:
         info_str += "]\n"
 
         info_str += f"board size: {self.board_size_x}x{self.board_size_y}\n"
+
+        info_str += f"cards: {self.INITIAL_CARDS_AMOUNT}\n"
+
+        for card in self.cards:
+            info_str += card.info() + '\n'
+
         return info_str
-
-
-class Game(GameEngine):
-    def __init__(self, cells_x: int, cells_y: int, name: str, game_id: str, players: Dict[str, Player], config: dict):
-        GameEngine.__init__(self,
-                            cells_x=cells_x,
-                            cells_y=cells_y,
-                            name=name,
-                            game_id=game_id,
-                            players=players,
-                            config=config)
-
-        self.__current_turn_num = 0
-        self.turn = list(self.players.values())[self.__current_turn_num].player_id
-
-        self.good_player = "Solider"
-        self.bad_player = "Saboteur"
-
-        for player_id, player_obj in players.items():
-            player_obj.player_role = self.good_player
-
-        self.cards_amount = 44 + 27  # 44 cards of tunnels and 27 cards of action
-
-        self.saboteur_amount = math.floor(22/(len(self.players)))
-
-        saboteurs = []
-        while len(saboteurs) < self.saboteur_amount:
-            player_id, player_obj = random.choice(list(self.players.items()))
-            if player_id not in saboteurs:
-                saboteurs.append(player_id)
-                player_obj.player_role = self.bad_player
-
-        x = len(self.players)
-        self.cards_per_player_amount = 6 if 3 <= x <= 5 else 4 if 6 <= x <= 10 else 0
-
-    def end_turn(self):
-        self.__current_turn_num = self.__current_turn_num + 1 if self.__current_turn_num < len(self.players) else 0
-        self.turn = list(self.players.values())[self.__current_turn_num].player_id
-        return f"turn ended, now its '{self.players[self.turn].player_name}' turn"
 
 
